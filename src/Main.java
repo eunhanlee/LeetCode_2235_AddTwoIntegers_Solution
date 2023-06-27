27aed302d41a5aedc5535a7e84e43a9ef133f89d
eunhanlee58 <eunhanlee58@gmail.com>
2023-06-27 17:16:53 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution tt= new Solution();
+        int num1 = 12;
+        int num2 = 5;
+
+        System.out.println(tt.sum(num1,num2));
+    }
+}
---
+++ src/Solution.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+    /**
+     * Returns the sum of two integers.
+     *
+     * @param num1 The first integer.
+     * @param num2 The second integer.
+     * @return The sum of num1 and num2.
+     */
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
